7b66e80a998dafce845cb073509a38908541adb2
UnambiguousUsername <dakota.glassburn@gmail.com>
2024-04-28 20:47:26 -0400

    Update project_setup.sh
    More /dev/null, who needs output?
    Beef up say
    Create venv directory and venv

--- project_setup.sh
+++ project_setup.sh
@@ -14,15 +14,19 @@ while getopts hlv option
 do
     case $option in
         h) echo "${help}"; exit 0;;
-        l) readonly log_file_path="$(dirname $(readlink -f "${0}"))/project_setup.log";;
+        l) readonly directory_path="$(dirname $(readlink -f "${0}"))";;
         v) verbose=true;;
         *) echo 'Unknown flags supplied.';;
     esac
-done 
+done
+
+log_file_path = "${directory_path}/project_setup.log"
+venv_path = "${directory_path}/venv/"
+venv_source_path = "${venv_path}/bin/activate"
 
 # printf function so i dont need to type newline for every string
 say(){
-    printf "$1\n"
+    { date +"%F %T" | tr -d "\n"; printf " - $1\n"; }
 }
 
 # check for sudo privilege
@@ -32,7 +36,7 @@ sudo true && say 'Nice.' || say 'Su privilege is necessary for me to work, bye.'
 # create log file
 if [ -v log_file_path ]; then
     if [ ! -f "${log_file_path}" ]; then
-        touch "${log_file_path}" &&
+        touch "${log_file_path}" &> /dev/null &&
             [[ "${verbose}" = true ]] && say 'Created log file.' && log=true ||
             [[ "${verbose}" = true ]] && say 'Unable to create log file.' && log=false
     else
@@ -52,7 +56,7 @@ if command -v python3 &> /dev/null; then
 else
     [[ "${verbose}" = true ]] && say 'Installing Python3.'
     [[ "${log}" = true ]] && say 'Installing Python3.' >> "${log_file_path}"
-    apt-get install -y python3.10
+    apt-get install -y python3.10 &> /dev/null
     if command -v python3 &> /dev/null; then
         [[ "${verbose}" = true ]] && say 'Python3 was installed.'
         [[ "${log}" = true ]] && say 'Python3 was installed.' >> "${log_file_path}"
@@ -69,7 +73,7 @@ if command -v pip &> /dev/null; then
 else
     [[ "${verbose}" = true ]] && say 'Installing Pip.'
     [[ "${log}" = true ]] && say 'Installing Pip.' >> "${log_file_path}"
-    apt-get install python3-pip
+    apt-get install python3-pip &> /dev/null
     if command -v pip &> /dev/null; then
         [[ "${verbose}" = true ]] && say 'Pip was installed.'
         [[ "${log}" = true ]] && say 'Pip was installed.' >> "${log_file_path}"
@@ -79,9 +83,6 @@ else
     fi
 fi
 
-# check for venv
-
-
 # check for postgresql
 if command -v psql &> /dev/null; then
     [[ "${verbose}" = true ]] && say 'Postgresql is installed.'
@@ -89,7 +90,7 @@ if command -v psql &> /dev/null; then
 else
     [[ "${verbose}" = true ]] && say 'Installing Postgresql.'
     [[ "${log}" = true ]] && say 'Installing Postgresql.' >> "${log_file_path}"
-    apt-get install -y postgresql
+    apt-get install -y postgresql &> /dev/null
     if command -v psql &> /dev/null; then
         [[ "${verbose}" = true ]] && say 'Postgresql was installed.'
         [[ "${log}" = true ]] && say 'Postgresql was installed.' >> "${log_file_path}"
@@ -99,6 +100,54 @@ else
     fi
 fi
 
+
+# check for venv directory
+if [ -d "${venv_path}" ]; then
+    [[ "${verbose}" = true ]] && say 'Venv directory found.'
+    [[ "${log}" = true ]] && say 'Venv directory found.' >> "${log_file_path}"
+else
+    [[ "${verbose}" = true ]] && say 'Creating venv directory.'
+    [[ "${log}" = true ]] && say 'Creating venv directory.' >> "${log_file_path}"
+    mkdir "${venv_path}" &> /dev/null
+    if [ -d "${venv_path}" ]; then
+        [[ "${verbose}" = true ]] && say 'Venv directory created.'
+        [[ "${log}" = true ]] && say 'Venv directory created.' >> "${log_file_path}"
+    else
+        [[ "${verbose}" = true ]] && say 'Failed to create venv directory. Exiting.'
+        [[ "${log}" = true ]] && say 'Failed to create venv directory. Exiting.' >> "${log_file_path}"
+        exit 1
+    fi
+fi
+
+# check for venv
+if [ -d "${venv_source_path}" ]; then
+    [[ "${verbose}" = true ]] && say 'Venv found.'
+    [[ "${log}" = true ]] && say 'Venv found.' >> "${log_file_path}"
+else
+    [[ "${verbose}" = true ]] && say 'Creating venv.'
+    [[ "${log}" = true ]] && say 'Creating venv.' >> "${log_file_path}"
+    python3 -m venv "${venv_path}" &> /dev/null
+    if [ -d "${venv_source_path}" ]; then
+        [[ "${verbose}" = true ]] && say 'Venv created.'
+        [[ "${log}" = true ]] && say 'Venv created.' >> "${log_file_path}"
+    else
+        [[ "${verbose}" = true ]] && say 'Failed to create venv. Exiting.'
+        [[ "${log}" = true ]] && say 'Failed to create venv. Exiting.' >> "${log_file_path}"
+        exit 1
+    fi
+fi
+
+# switch to venv
+source "${venv_source_path}" &> /dev/null
+if command -v python3 | grep "${venv_source_path}" &> /dev/null; then
+    [[ "${verbose}" = true ]] && say 'Switch to venv successful.'
+    [[ "${log}" = true ]] && say 'Switch to venv successful.' >> "${log_file_path}"
+else
+    [[ "${verbose}" = true ]] && say 'Switch to venv failed. Exiting.'
+    [[ "${log}" = true ]] && say 'Switch to venv failed. Exiting.' >> "${log_file_path}"
+    exit 1
+fi
+
 # check for apache airflow
 if command -v airflow &> /dev/null; then
     [[ "${verbose}" = true ]] && say 'Apache Airflow is installed.'
@@ -106,7 +155,7 @@ if command -v airflow &> /dev/null; then
 else
     [[ "${verbose}" = true ]] && say 'Installing Apache Airflow.'
     [[ "${log}" = true ]] && say 'Installing Apache Airflow.' >> "${log_file_path}"
-    python3 -m pip install apache-airflow
+    python3 -m pip install apache-airflow &> /dev/null
     if command -v airflow &> /dev/null; then
         [[ "${verbose}" = true ]] && say 'Apache Airflow was installed.'
         [[ "${log}" = true ]] && say 'Apache Airflow was installed.' >> "${log_file_path}"
@@ -121,5 +170,5 @@ fi
 
 # apache airflow setup
 
-
+deactivate
 say 'fin'
